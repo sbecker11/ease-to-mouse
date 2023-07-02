8e3fe7b08b76a8f77f78d062fbd5784d7cdbc487
Michael <216956+mikez@users.noreply.github.com>
2023-07-02 10:58:31 +0200

    Update focal_point.js

--- modules/focal_point.js
+++ modules/focal_point.js
@@ -1,49 +1,109 @@
+const EASING = 0.05;
+const EPSILON = EASING / 2.0;
+
 var _canvasContainer;
-var _focalPoint;
+var _focalPointElement;
+var _focalPointNowSubpixelPrecision;
+var _focalPointAim;
 var _focalPointListener;
+var _isAnimating;
 
 // -----------------------------------------------------
-// save the caller's canvasContainer and focalPoint elements.
+// save the caller's canvasContainer and focalPointElement.
 // save the caller's callback function, focalPointListener,
 // which will be called while the focalPoint is moving.
 //
-export function createFocalPoint(canvasContainer, focalPoint, focalPointListener) {
-  _canvasContainer = canvasContainer;
-  _focalPoint = focalPoint;
-  _focalPointListener = focalPointListener;
+export function createFocalPoint(
+    canvasContainer,
+    focalPointElement,
+    focalPointListener
+) {
+    _canvasContainer = canvasContainer;
+    _focalPointElement = focalPointElement;
+    _focalPointNowSubpixelPrecision = getFocalPoint();
+    _focalPointListener = focalPointListener;
+    _isAnimating = false;
 }
 
 // get the canvasContainer-relative
-// location of the _focalPoint center
+// location of the _focalPointElement center
 export function getFocalPoint() {
-  var fpX = _focalPoint.offsetLeft + Math.floor(_focalPoint.offsetWidth / 2);
-  var fpY = _focalPoint.offsetTop + Math.floor(_focalPoint.offsetHeight / 2);
-  return { fpX, fpY };
+    return {
+        x:
+            _focalPointElement.offsetLeft
+            + Math.floor(_focalPointElement.offsetWidth / 2),
+        y:
+            _focalPointElement.offsetTop
+            + Math.floor(_focalPointElement.offsetHeight / 2),
+    };
 }
 
-// move the _focalPoint to the given
+// move the _focalPointElement to the given
 // canvasContainer-relative location
 export function moveFocalPointTo(x, y) {
-  var newLeft = x - Math.floor(_focalPoint.offsetWidth / 2);
-  var newTop = y - Math.floor(_focalPoint.offsetHeight / 2);
-  _focalPoint.style.left = `${newLeft}px`;
-  _focalPoint.style.top = `${newTop}px`;
-  // alternative
-  // see https://stackoverflow.com/a/53892597
-  // _focalPoint.style.transform = `translate(${x}px, ${y}px)`;
+    var newLeft = x - Math.floor(_focalPointElement.offsetWidth / 2);
+    var newTop = y - Math.floor(_focalPointElement.offsetHeight / 2);
+    _focalPointElement.style.left = `${newLeft}px`;
+    _focalPointElement.style.top = `${newTop}px`;
+    // alternative
+    // see https://stackoverflow.com/a/53892597
+    // _focalPointElement.style.transform = `translate(${x}px, ${y}px)`;
 
-  // notify the caller's listener
-  _focalPointListener(x, y);
+    // notify the caller's listener
+    _focalPointListener(x, y);
 }
 
-// ease the _focalPoint to the given
+// ease the _focalPointElement to the given
 // canvasContainer-relative location
 export function easeFocalPointTo(x, y, callback) {
-  // for now this is just a direct move, but this function
-  // needs to be altered to animate the focalPoint
-  moveFocalPointTo(x, y);
+    _isAnimating = true;
 
-  // notify the caller's listener at each
-  // frame in the animation
-  _focalPointListener(x, y);
+    // for now this is just a direct move, but this function
+    // needs to be altered to animate the focalPoint
+    // moveFocalPointTo(x, y);
+    _focalPointAim = { x, y };
+
+    // notify the caller's listener at each
+    // frame in the animation
+    // _focalPointListener(x, y);
+}
+
+export function drawFocalPointAnimationFrame() {
+    if (!_isAnimating) return;
+
+    const focalPointNow = getFocalPoint();
+
+    // exit early if we're at the destination already
+    if (focalPointNow.x === _focalPointAim.x && focalPointNow.y === _focalPointAim.y) {
+        _isAnimating = false;
+        return;
+    }
+
+    _focalPointNowSubpixelPrecision = computeAStepClosertoAimSubpixelPrecision(
+        _focalPointNowSubpixelPrecision,
+        _focalPointAim,
+        EASING,
+        EPSILON
+    );
+
+    moveFocalPointTo(
+        Math.round(_focalPointNowSubpixelPrecision.x),
+        Math.round(_focalPointNowSubpixelPrecision.y)
+    );
+}
+
+function computeAStepClosertoAimSubpixelPrecision(nowPoint, aimPoint, easing, epsilon) {
+    // compute velocities
+    let vx = (aimPoint.x - nowPoint.x) * easing;
+    let vy = (aimPoint.y - nowPoint.y) * easing;
+
+    // for very small values of vx and vy, move there directly
+    if (Math.abs(vx) < epsilon && Math.abs(vy) < epsilon) {
+        return aimPoint;
+    }
+
+    return {
+        x: nowPoint.x + vx,
+        y: nowPoint.y + vy,
+    };
 }
